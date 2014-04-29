4997e6b2bea416bc3965b097f28e74501f8f69fe
David Tanner <darthtanner@gmail.com>
2014-04-28 23:33:47 -0600

    Initial Commit.

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: David Tanner
+ * Date: 4/28/14
+ * Time: 10:58 PM
+ */
+
+?>
+
+<html>
+<head>
+    <title>Professional Me</title>
+
+    <link rel="shortcut icon" type="image/x-icon" href="FAVICON.ICO">
+    <link href="css/bootstrap-theme.min.css" type="text/css">
+    <link href="css/bootstrap.min.css" type="text/css">
+
+    <script type="text/javascript" src="js/jquery-2.1.0.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+    <style type="text/css">
+        @font-face {
+            font-family: "STAR_WARS";
+            src: url("fonts/starjout/Starjout.ttf");
+        }
+        #title {
+            font-family: "STAR_WARS", sans-serif;
+            font-size:72px;
+            text-align: center;
+            color: #1c94c4;
+        }
+        body {
+            margin: 0 auto; /* centers page */
+            background: #fff url(http://www.nasa.gov/images/content/648794main_PIA15625_full.jpg) no-repeat center scroll;
+            background-size: 2000px 1000px;
+        }
+
+    </style>
+</head>
+<body>
+    <div id="title">This is my Resume site<br />To be continued...</div>
+</body>
+</html>
