831f74d5f7069bf4c828852c82cb9b24f45ca3e7
David Tanner <darthtanner@gmail.com>
2014-04-29 23:17:03 -0600

    cleaning up the home page.

--- index.php
+++ index.php
@@ -7,38 +7,65 @@
  */
 
 ?>
-
+<!DOCTYPE html>
 <html>
 <head>
     <title>Professional Me</title>
 
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+
     <link rel="shortcut icon" type="image/x-icon" href="FAVICON.ICO">
     <link href="css/bootstrap-theme.min.css" type="text/css">
     <link href="css/bootstrap.min.css" type="text/css">
+    <link href='http://fonts.googleapis.com/css?family=Paytone+One' rel='stylesheet' type='text/css'>
 
     <script type="text/javascript" src="js/jquery-2.1.0.min.js"></script>
     <script type="text/javascript" src="js/bootstrap.min.js"></script>
 
     <style type="text/css">
-        @font-face {
-            font-family: "STAR_WARS";
-            src: url("fonts/starjout/Starjout.ttf");
-        }
+
         #title {
-            font-family: "STAR_WARS", sans-serif;
-            font-size:72px;
             text-align: center;
-            color: #1c94c4;
+            font-family: Verdana, serif;
+            color: #00275d;
         }
-        body {
-            margin: 0 auto; /* centers page */
-            background: #fff url(http://www.nasa.gov/images/content/648794main_PIA15625_full.jpg) no-repeat center scroll;
-            background-size: 2000px 1000px;
+        #title h1, h2, h3, h4 {
+            margin: 0 0 0 0;
+        }
+
+        #title h4 {
+            display: inline-block;
+        }
+
+        #title a {
+            color: inherit;
+        }
+
+        #title h1 span {
+            color: white;
+            background-color: #00275d;
+            text-shadow:
+                -.025em -.025em 0 #dab37b,
+                .025em -.025em 0 #dab37b,
+                -.025em .025em 0 #dab37b,
+                .025em .025em 0 #dab37b;
+        }
+
+        @media only screen and (max-width: 600px) {
+            .hide-small {
+                display: none !important;
+            }
         }
 
     </style>
 </head>
 <body>
-    <div id="title">This is my Resume site<br />To be continued...</div>
+<div id="title">
+    <h1><span>&nbsp;David Tanner&nbsp;</span></h1>
+    <h4>CELL&nbsp;<a href="tel:(801) 810-6443">(801) 810-6443</a></h4>
+    <h4 class="hide-small">&nbsp;|&nbsp;</h4>
+    <h4>EMAIL&nbsp;<a href="mailto:david.joel.tanner@gmail.com">david.joel.tanner@gmail.com</a></h4>
+</div>
+
 </body>
 </html>
